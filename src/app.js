e76d3c3140a108eab3226bbd44d2f2ead61067c9
mah-qurashy <diz49@live.com>
2020-11-12 03:33:22 +0200

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./geocode')
 const getweather = require('./getweather')
 
 const app = express()
-
+const port =  process.env.PORT || 3000
 //setup handlebars engine
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, '../templates/views'))
@@ -75,6 +75,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
